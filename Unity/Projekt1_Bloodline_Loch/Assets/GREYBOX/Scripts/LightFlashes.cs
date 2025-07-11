423f9da0cc90e7388e8478e0ba3928af1d8cdb81
HerbeAmAbgehen <sebastian-loch@outlook.de>
2025-07-11 11:39:36 +0200

    Update Unity
    Szenen aufgeräumt
    Lightning Script gefixt
    Body Piles Dcoration angefangen

--- Unity/Projekt1_Bloodline_Loch/Assets/GREYBOX/Scripts/LightFlashes.cs
+++ Unity/Projekt1_Bloodline_Loch/Assets/GREYBOX/Scripts/LightFlashes.cs
@@ -22,31 +22,34 @@ public class LightFlashes : MonoBehaviour
 
         DefaultIntensity = DirectionalLight.intensity;
 
-        InvokeRepeating("Lightning", 1, 7);
+        InvokeRepeating("Lightning", 1, 4);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void Lightning()
     {
-        LightIntensity = Random.Range(0.4f, 0.7f);
-
-        DirectionalLight.intensity = LightIntensity;
-
-        StartCoroutine("FlashTimer");
-
-        DirectionalLight.intensity = DefaultIntensity;
-
-        Debug.Log("FLASH");
-
+        LightIntensity = Random.Range(0.3f, 0.7f);
+        DirectionalLight.intensity = LightIntensity;  
+        StartCoroutine(FlashTimer());
+        RandomLight = Random.Range(0f, 10f);
+        
+        
     }
 
     IEnumerator FlashTimer()
     {
         yield return new WaitForSeconds(FlashTime);
+
+        DirectionalLight.intensity = DefaultIntensity;
+
+        if (RandomLight > 3f)
+        {
+            Lightning();
+        }
     }
 }
