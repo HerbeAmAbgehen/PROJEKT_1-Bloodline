f00fc4e6f0dea5b9c3be1936ac477ea704930377
HerbeAmAbgehen <sebastian-loch@outlook.de>
2025-07-12 15:23:17 +0200

    CAT PAT Update Unity
    Cat is now pattable!!!

---
+++ Unity/Projekt1_Bloodline_Loch/Assets/GREYBOX/Scripts/PatCat.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PatCat : MonoBehaviour
+{
+    public GameObject Arm;
+    public GameObject PatText;
+
+    public GameObject Player;
+    public GameObject PlayerCameraRoot;
+    public GameObject PlayerPositionTarget;
+
+    public AnimationClip Pat;
+    public AnimationClip Scratch;
+    public AnimationClip Look;
+    public AnimationClip ArmPat;
+    
+    private Animator Animator;
+    private Animator ArmAnimator;
+
+    private CharacterController PC;
+    
+    private float PlayerRotation = 268.5f;
+    private float CameraRootRotation = 30f;
+
+    private bool CanBePatted;
+    private bool IsScratching;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        ArmAnimator = Arm.GetComponent<Animator>();
+        PC = Player.GetComponent<CharacterController>();
+
+        Arm.gameObject.SetActive(false);
+        InvokeRepeating("PlayScratch", 15, 25);
+
+        IsScratching = false;
+        PatText.SetActive(false);
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (IsScratching)
+        {
+            CanBePatted = false;
+            PatText.SetActive(false);
+        }
+        else if (!IsScratching)
+        {
+            CanBePatted = true;
+            PatText.SetActive(true);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.E) && !IsScratching) 
+        {
+            MovePlayer();
+            StartCoroutine(CatPat());
+            Debug.Log("Trigger: Pat");           
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CanBePatted = false;
+    }
+
+    private void PlayScratch()
+    {
+        StartCoroutine(CatScratch());
+        Debug.Log("Trigger: Scratch");
+    }
+
+    private IEnumerator CatPat()
+    {
+        
+        CanBePatted=false;
+        Arm.gameObject.SetActive(true);
+        Animator.SetTrigger("Pat");
+        yield return new WaitForSeconds(2f);
+        ArmAnimator.SetTrigger("Pat");
+        yield return new WaitForSeconds(ArmPat.length);
+        ArmAnimator.SetTrigger("Idle");
+        Arm.gameObject.SetActive(false);
+        yield return new WaitForSeconds(Pat.length - ArmPat.length);
+        Animator.SetTrigger("Idle");
+        PC.gameObject.SetActive(true);
+        CanBePatted = true;
+    }
+
+    private IEnumerator CatScratch()
+    {
+        IsScratching = true;
+        Animator.SetTrigger("Scratch");
+        yield return new WaitForSeconds(Scratch.length);
+        Animator.SetTrigger("Idle");
+        yield return new WaitForSeconds(2);
+        IsScratching = false;
+    }
+
+    private void MovePlayer()
+    {
+        PC.gameObject.SetActive(false);
+        Player.transform.position = PlayerPositionTarget.transform.position;
+        Player.transform.localEulerAngles = new Vector3(0, PlayerRotation, 0);
+        PlayerCameraRoot.transform.localEulerAngles = new Vector3 (CameraRootRotation,0,0);
+        Debug.Log("MovedPlayer");
+    }
+}
